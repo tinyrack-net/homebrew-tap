d69afd27a6a3add97859a70ee2f002b46d11243e
winetree94 <me@winetree94.com>
2026-08-12 14:06:20 +0900

    Fix tinest-cli release URLs

--- Formula/tinest-cli.rb
+++ Formula/tinest-cli.rb
@@ -5,18 +5,18 @@ class TinestCli < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/tinyrack-net/tinest/releases/download/v0.7.0/tinest-cli-macos-x64.tar.gz"
+      url "https://github.com/tinyrack-net/coder/releases/download/v0.7.0/tinest-cli-macos-x64.tar.gz"
       sha256 "052aa0d8df7bf9580cfb90c28d16998fbc41192996f2ac2dd57bee09517fa5ad"
     end
     on_arm do
-      url "https://github.com/tinyrack-net/tinest/releases/download/v0.7.0/tinest-cli-macos-arm64.tar.gz"
+      url "https://github.com/tinyrack-net/coder/releases/download/v0.7.0/tinest-cli-macos-arm64.tar.gz"
       sha256 "357362b077b21d269c06d69503403e4fc624fc070d6ee8c52be042121e2edb2f"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/tinyrack-net/tinest/releases/download/v0.7.0/tinest-cli-linux-x64.tar.gz"
+      url "https://github.com/tinyrack-net/coder/releases/download/v0.7.0/tinest-cli-linux-x64.tar.gz"
       sha256 "e745e77afea091e8127061aa27cd9319b2a2840cabed9807b61505fdef45fdf8"
     end
   end
